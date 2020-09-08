44da161a12945e22f1cda88d942e731336693198
seanmcquaid <sean.mcquaid2@verizon.net>
2020-09-07 22:15:32 -0400

    added in initial project for the back end

---
+++ server/src/test/java/com/wackamole/api/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wackamole.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
