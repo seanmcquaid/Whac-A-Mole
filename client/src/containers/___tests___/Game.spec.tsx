67eae1960c2816b14741266f40b7a20e422a4f16
seanmcquaid <sean.mcquaid2@verizon.net>
2020-09-09 22:31:25 -0400

    sketched game spec

---
+++ client/src/containers/___tests___/Game.spec.tsx
@@ -0,0 +1,11 @@
+describe('<Game/>', () => {
+    it('Can click start button to start the game', () => {})
+
+    it('Timer starts when you hit start', () => {})
+
+    it('Every time you hit a mole, another will appear', () => {})
+
+    it('Game should end when the user hits 15 moles', () => {})
+
+    it('User is prompted to enter their time if it is in the top 10', () => {})
+})
