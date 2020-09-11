8ac9b82d1886d651607d17c1796b58b5a2692e8b
seanmcquaid <sean.mcquaid2@verizon.net>
2020-09-11 17:16:50 -0400

    set up time accumulator

--- client/src/containers/Game/GameBoard.tsx
+++ client/src/containers/Game/GameBoard.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 
 const GameBoard = () => {
   const dispatch = useDispatch();
+  const [timeTotal, setTimeTotal] = useState(0);
+
+  console.log(timeTotal);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeTotal((time) => time + 1000);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return <GameContainer></GameContainer>;
 };
 
