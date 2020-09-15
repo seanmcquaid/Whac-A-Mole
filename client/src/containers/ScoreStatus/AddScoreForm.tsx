0f6bb9cb30a395379991bfc7e9f81bb0e8f0e73a
seanmcquaid <sean.mcquaid2@verizon.net>
2020-09-14 21:50:09 -0400

    added in request on add score form

--- client/src/containers/ScoreStatus/AddScoreForm.tsx
+++ client/src/containers/ScoreStatus/AddScoreForm.tsx
@@ -2,10 +2,30 @@ import React, { useCallback } from 'react';
 import { Formik, Form, Field } from 'formik';
 import Axios from 'axios';
 import Button from '../../sharedComponents/universal/Button';
+import { useSelector } from 'react-redux';
+import { totalTimeSelector } from '../../store/game/selectors';
 
 const AddScoreForm = React.memo(() => {
+  const score = useSelector(totalTimeSelector);
+
   const addScoreOnSubmit = useCallback((name) => {
-    console.log(name);
+    const { REACT_APP_API_URL } = process.env;
+    const source = Axios.CancelToken.source();
+    const config = {
+      cancelToken: source.token,
+    };
+
+    const requestBody = {
+      name,
+      score,
+    };
+
+    Axios.post(`${REACT_APP_API_URL}/v1/scores/addScore`, requestBody, config)
+      .then(({ data }) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {});
   }, []);
 
   return (
@@ -14,7 +34,7 @@ const AddScoreForm = React.memo(() => {
       onSubmit={({ name }) => addScoreOnSubmit(name)}
     >
       <Form>
-        <Field name="name" type="text" />
+        <Field name="name" type="text" placeholder="Enter name here" />
         <Button type="submit" name="Submit" />
       </Form>
     </Formik>
