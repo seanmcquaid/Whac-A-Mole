3b3d2151bf482ed56cf414470f13ad3cc2563972
seanmcquaid <sean.mcquaid2@verizon.net>
2020-09-16 21:37:03 -0400

    added mole tests

--- client/src/sharedComponents/app/Game/___tests___/Mole.spec.tsx
+++ client/src/sharedComponents/app/Game/___tests___/Mole.spec.tsx
@@ -1,6 +1,29 @@
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import Mole from '../Mole';
 
 describe('<Mole/>', () => {
-  it('should ', () => {});
+  it('Visible', () => {
+    const props = {
+      isVisible: true,
+      onClick: jest.fn(),
+    };
+
+    render(<Mole {...props} />);
+
+    expect(screen.getByTestId('visibleMole')).toBeInTheDocument();
+    expect(screen.queryByTestId('hiddenMole')).toBeNull();
+  });
+
+  it('Hidden', () => {
+    const props = {
+      isVisible: false,
+      onClick: jest.fn(),
+    };
+
+    render(<Mole {...props} />);
+
+    expect(screen.getByTestId('hiddenMole')).toBeInTheDocument();
+    expect(screen.queryByTestId('visibleMole')).toBeNull();
+  });
 });
