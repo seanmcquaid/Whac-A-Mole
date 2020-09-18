6cc4bc771a05bd311ee9ec614fd52ce429bd2985
seanmcquaid <sean.mcquaid2@verizon.net>
2020-09-17 23:24:36 -0400

    added in leaderboard test" git push

--- client/src/containers/___tests___/LeaderBoard.spec.tsx
+++ client/src/containers/___tests___/LeaderBoard.spec.tsx
@@ -1,6 +1,36 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import Axios from 'axios';
 import React from 'react';
+import { Route } from 'react-router-dom';
+import { globalState } from '../../store';
+import MockRouter from '../../testUtils/MockRouter';
+import MockStore from '../../testUtils/MockStore';
 import LeaderBoard from '../LeaderBoard';
 
 describe('<LeaderBoard/>', () => {
-  it('Displays the top 10 times', () => {});
+  it('Displays the top 10 times', async () => {
+    const initialState: globalState = {
+      game: {
+        molesLeft: 15,
+        gameActive: false,
+        totalTime: 0,
+      },
+    };
+
+    const initialRoute = '/leaderBoard';
+
+    jest.spyOn(Axios, 'get').mockResolvedValueOnce({
+      data: [{ name: 'Sean', score: 1000 }],
+    });
+
+    render(
+      <MockStore initialState={initialState}>
+        <MockRouter initialRoute={initialRoute}>
+          <Route exact path="/leaderBoard" component={LeaderBoard} />
+        </MockRouter>
+      </MockStore>
+    );
+
+    await waitFor(() => expect(screen.getByText('Sean -')).toBeInTheDocument());
+  });
 });
