8b4cbd092cdb717fdf3eda7c6a347107eb339950
seanmcquaid <sean.mcquaid2@verizon.net>
2020-09-18 21:00:50 -0400

    finished a couple actions tests

--- client/src/store/___tests___/game/actions.spec.ts
+++ client/src/store/___tests___/game/actions.spec.ts
@@ -1,14 +1,43 @@
+import { GlobalState } from '../..';
+import createMockActionStore from '../../../testUtils/createMockActionStore';
 import {
   hitMole,
   startGame,
   incrementTimer,
   endGame,
 } from '../../game/actions';
+import GameActionTypes from '../../game/actionTypes';
 
 describe('gameActions', () => {
-  it('hitMole', () => {});
+  it('hitMole', () => {
+    const initialState: GlobalState = {
+      game: {
+        molesLeft: 15,
+        totalTime: 0,
+        gameActive: true,
+      },
+    };
+    const store = createMockActionStore(initialState);
 
-  it('startGame', () => {});
+    store.dispatch(hitMole());
+
+    expect(store.getActions()).toEqual([{ type: GameActionTypes.hitMole }]);
+  });
+
+  it('startGame', () => {
+    const initialState: GlobalState = {
+      game: {
+        molesLeft: 15,
+        totalTime: 0,
+        gameActive: true,
+      },
+    };
+    const store = createMockActionStore(initialState);
+
+    store.dispatch(startGame());
+
+    expect(store.getActions()).toEqual([{ type: GameActionTypes.startGame }]);
+  });
 
   it('incrementTimer', () => {});
 
