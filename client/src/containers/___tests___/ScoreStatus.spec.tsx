cbf6932ad5e8dabfa23c7c91c94cbdad09e762a8
seanmcquaid <sean.mcquaid2@verizon.net>
2020-09-18 20:28:52 -0400

    sketched out tests for scorestatus

--- client/src/containers/___tests___/ScoreStatus.spec.tsx
+++ client/src/containers/___tests___/ScoreStatus.spec.tsx
@@ -2,5 +2,11 @@ import React from 'react';
 import ScoreStatus from '../ScoreStatus';
 
 describe('<ScoreStatus/>', () => {
-  it('should ', () => {});
+  it('Should display if score is in the top ten', () => {});
+
+  it("Should display if score isn't the top ten", () => {});
+
+  it('Should allow the user to enter their name and submit if their score is in the top 10', () => {});
+
+  it("Should allow the user to go back home if their score isn't in the top 10", () => {});
 });
