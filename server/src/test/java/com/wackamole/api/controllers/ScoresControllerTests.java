4f0359f560a9b0628888e5204972a08fbfe74a70
seanmcquaid <sean.mcquaid2@verizon.net>
2020-09-19 22:03:10 -0400

    finished controller test

--- server/src/test/java/com/wackamole/api/controllers/ScoresControllerTests.java
+++ server/src/test/java/com/wackamole/api/controllers/ScoresControllerTests.java
@@ -1,25 +1,94 @@
 package com.wackamole.api.controllers;
 
-import org.assertj.core.api.Assert;
+import com.wackamole.api.dao.ScoresDao;
+import com.wackamole.api.models.Score;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @SpringBootTest
 public class ScoresControllerTests {
 
+    @Autowired
+    ScoresController scoresController;
+
+    @Autowired
+    ScoresDao scoresDao;
+
+    private final Score scoreForTest = new Score("User 1", 2000);
+
     @Test
     public void getLeaderBoardTest(){
+        List<Score> topTenScores = new ArrayList<>();
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
 
+        topTenScores.forEach(score -> {
+            scoresDao.addScore(score);
+        });
+
+        ResponseEntity<List<Score>> getLeaderBoardResponse = scoresController.getLeaderBoard();
+
+        Assert.assertEquals(200, getLeaderBoardResponse.getStatusCodeValue());
+        Assert.assertEquals(10, Objects.requireNonNull(getLeaderBoardResponse.getBody()).size());
+        Assert.assertEquals(topTenScores, getLeaderBoardResponse.getBody());
+
+        topTenScores.forEach(score -> {
+            scoresDao.deleteScore(score);
+        });
     }
 
     @Test
     public void addScoreTest(){
+        ResponseEntity<List<Score>> addScoreResponse = scoresController.addScore(scoreForTest);
 
+        Assert.assertEquals(200, addScoreResponse.getStatusCodeValue());
+        Assert.assertEquals(1, Objects.requireNonNull(addScoreResponse.getBody()).size());
+
+        scoresDao.deleteScore(scoreForTest);
     }
 
     @Test
     public void checkTime(){
+        List<Score> topTenScores = new ArrayList<>();
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
+        topTenScores.add(new Score("User 1", 3000));
 
+        topTenScores.forEach(score -> {
+            scoresDao.addScore(score);
+        });
+
+        ResponseEntity<Boolean> checkTimeResponse = scoresController.checkTime(scoreForTest.getScore());
+
+        Assert.assertEquals(200, checkTimeResponse.getStatusCodeValue());
+
+        Assert.assertTrue(Objects.requireNonNull(checkTimeResponse.getBody()));
+
+        topTenScores.forEach(score -> {
+            scoresDao.deleteScore(score);
+        });
     }
 }
